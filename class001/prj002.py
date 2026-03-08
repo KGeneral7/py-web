57cd1bf0381dc0e04fa69610a1a2501ca0af3699
Kao197 <yuntse197@gmail.com>
2026-03-08 10:57:44 +0800

    新增 prj001.py 和 prj002.py，實作基本 GUI 功能與按鈕事件

--- class001/prj002.py
+++ class001/prj002.py
@@ -5,18 +5,23 @@ from tkinter import *
 #######################定義函數########################
 def hi():
     print("Hello, world!")
+def show():
+    display.config(text="Hello, world!", fg="blue", bg="red")#設定標籤的文字為"Hello, world!"
+def clear():
+    display.config(text="")#設定標籤的文字為空字串，清除標籤的內容
 #######################建立視窗########################
 window = Tk() #建立視窗物件
 window.title("My first GUI") #設定視窗標題
 window.geometry("400x300") #設定視窗大小，格式為"寬x高"
 #######################建立按鈕########################
-button = Button(window, text="Click me!", command=hi) #建立按鈕物件，並設定按鈕文字
+button1 = Button(window, text="show", command=show) #建立按鈕物件，並設定按鈕文字
 #指令(def)在這裡裡面不能加()因為我們只是要傳遞函數的參考，而不是呼叫函數
-button.pack() #將按鈕放入視窗中，並自動調整位置和大小
+button2 = Button(window, text="clear", command=clear)
 #######################建立標籤########################
-display = Label(window, text="Welcome to my GUI!", fg="red", bg="black") #建立標籤物件，並設定標籤文字
+display = Label(window, text="") #建立標籤物件，並設定標籤文字
 #######################運行應用程式########################
-button.pack() #將按鈕放入視窗中，並自動調整位置和大小
+button1.pack() #將按鈕放入視窗中，並自動調整位置和大小
+button2.pack() #將按鈕放入視窗中，並自動調整位置和大小
 display.pack() #將標籤放入視窗中，並自動調整位置和大小
 window.mainloop() 
 '''
